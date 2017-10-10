199a8d1b87cf5205dca76bc85f862b7f24af61e6
alan199355 <2271088240@qq.com>
2017-10-10 22:43:59 +0800

    node practice

--- nodeSchool/program.js
+++ nodeSchool/program.js
@@ -47,10 +47,103 @@
 //     })
 // })
 
-const http = require('http')
+// const http = require('http')
 
-http.get(process.argv[2], function (response) {
-  response.setEncoding('utf8')
-  response.on('data', console.log)
-  response.on('error', console.error)
-}).on('error', console.error)
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// }).on('error', console.error)
+
+// const http = require('http')
+// const bl = require('bl')
+
+// http.get(process.argv[2], function (response) {
+//   response.pipe(bl(function (err, data) {
+//     if (err) {
+//       return console.error(err)
+//     }
+//     data = data.toString()
+//     console.log(data.length)
+//     console.log(data)
+//   }))
+// })
+
+// const http = require('http')
+// const bl = require('bl')
+// const results = []
+// let count = 0
+
+// function printResults () {
+//   for (let i = 0; i < 3; i++) {
+//     console.log(results[i])
+//   }
+// }
+
+// function httpGet (index) {
+//   http.get(process.argv[2 + index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err) {
+//         return console.error(err)
+//       }
+
+//       results[index] = data.toString()
+//       count++
+
+//       if (count === 3) {
+//         printResults()
+//       }
+//     }))
+//   })
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   httpGet(i)
+// }
+
+// const net = require('net')
+
+// function zeroFill (i) {
+//   return (i < 10 ? '0' : '') + i
+// }
+
+// function now () {
+//   const d = new Date()
+//   return d.getFullYear() + '-' +
+//     zeroFill(d.getMonth() + 1) + '-' +
+//     zeroFill(d.getDate()) + ' ' +
+//     zeroFill(d.getHours()) + ':' +
+//     zeroFill(d.getMinutes())
+// }
+
+// const server = net.createServer(function (socket) {
+//   socket.end(now() + '\n')
+// })
+
+// server.listen(Number(process.argv[2]))
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer(function (req, res) {
+//   res.writeHead(200, { 'content-type': 'text/plain' })
+
+//   fs.createReadStream(process.argv[3]).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
+
+const http = require('http')
+const map = require('through2-map')
+
+const server = http.createServer(function (req, res) {
+  if (req.method !== 'POST') {
+    return res.end('send me a POST\n')
+  }
+
+  req.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
