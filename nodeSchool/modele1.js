f5af61857a4e3ee7b6676b141e6dd4bab6565e86
alan199355 <2271088240@qq.com>
2017-10-31 21:30:49 +0800

    express

--- nodeSchool/modele1.js
+++ nodeSchool/modele1.js
@@ -12,3 +12,21 @@ module.exports = function (folder, ext, callback) {
         callback(null, lists)
     })
 }
+var z=10;
+function foo(){
+    console.log(z);
+}
+(function(funArg){
+    var z=20;
+    funArg();
+})(foo)
+
+var data=[];
+for(var k=0;k<3;k++){
+    data[k]=function(){
+        console.log(k);
+    };
+}
+data[0]();
+data[1]();
+data[2]();
--- nodeSchool/program.js
+++ nodeSchool/program.js
@@ -133,17 +133,53 @@
 
 // server.listen(Number(process.argv[2]))
 
+// const http = require('http')
+// const map = require('through2-map')
+
+// const server = http.createServer(function (req, res) {
+//   if (req.method !== 'POST') {
+//     return res.end('send me a POST\n')
+//   }
+
+//   req.pipe(map(function (chunk) {
+//     return chunk.toString().toUpperCase()
+//   })).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
+
 const http = require('http')
-const map = require('through2-map')
+const url = require('url')
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime: time.getTime() }
+}
 
 const server = http.createServer(function (req, res) {
-  if (req.method !== 'POST') {
-    return res.end('send me a POST\n')
+  const parsedUrl = url.parse(req.url, true)
+  const time = new Date(parsedUrl.query.iso)
+  let result
+
+  if (/^\/api\/parsetime/.test(req.url)) {
+    result = parsetime(time)
+  } else if (/^\/api\/unixtime/.test(req.url)) {
+    result = unixtime(time)
   }
 
-  req.pipe(map(function (chunk) {
-    return chunk.toString().toUpperCase()
-  })).pipe(res)
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(result))
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
 })
-
 server.listen(Number(process.argv[2]))
