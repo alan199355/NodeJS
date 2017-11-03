e1f3880c0f3de23b86f07f6f8a23793e9cbca869
alan199355 <2271088240@qq.com>
2017-11-03 23:41:00 +0800

    test

--- myExpress/views/src/app/Services/account.service.ts
+++ myExpress/views/src/app/Services/account.service.ts
@@ -1,4 +1,6 @@
 import { HttpModule, Headers, Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+@Injectable()
 export class AccountService {
   private headers = new Headers({ 'Content-Type': 'application/json' });
   constructor(
