7856b7804e65ee5620b2ab81e77a29f97c0eb2eb
alan199355 <2271088240@qq.com>
2018-01-15 22:13:51 +0800

    添加redux

--- Koa/views/index.js
+++ Koa/views/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppRouter from './router'
-
+import AppRouter from './router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 // axios({
 //     method:'POST',
