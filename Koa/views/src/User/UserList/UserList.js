374d0df8759ec30439482ce8ed8d9e928814da1f
alan199355 <2271088240@qq.com>
2018-09-26 22:20:58 +0800

    同步代码

--- Koa/views/src/User/UserList/UserList.js
+++ Koa/views/src/User/UserList/UserList.js
@@ -1,42 +1,50 @@
-import React from 'react';
-import { Table, Divider, Tag } from 'antd';
+import React from "react";
+import { Table, Divider, Tag,Button } from "antd";
 const { Column, ColumnGroup } = Table;
-import {userAPI} from '../../services/user'
+import { userAPI } from "../../services/user";
 
 class UserList extends React.Component {
   constructor() {
     super();
-    this.state = { userList:[] };
+    this.state = { userList: [] };
   }
 
   render() {
     return (
-        <Table dataSource={this.state.userList}>
-           
-            <Column title="username" dataIndex="username" key="username" />
-            <Column title="password" dataIndex="password" key="password" />
-            <Column title="email" dataIndex="email" key="email" />
-
-        </Table>
-    )
+      <Table dataSource={this.state.userList}>
+        <Column title="username" dataIndex="username" key="username" />
+        <Column title="password" dataIndex="password" key="password" />
+        <Column title="email" dataIndex="email" key="email" />
+        <Column
+          title="Action"
+          key="action"
+          dataIndex="action"
+          render={record => (
+            <span>
+              <a href="javascript:;">编辑</a>
+              <Divider type="vertical" />
+              <a href="javascript:;">删除</a>
+            </span>
+          )}
+        />
+      </Table>
+    );
   }
 
   componentDidMount() {
     // this.setState({ someKey: 'otherValue' });
-    this.getUserList=this.getUserList.bind(this)
-    this.getUserList()
+    this.getUserList = this.getUserList.bind(this);
+    this.getUserList();
   }
 
-  async getUserList(){
-      try {
-        let res=await userAPI.getUserList()
-        this.setState({
-            userList:res.data
-        })
-        console.log(this.state.userList,'adsdas')
-      } catch (error) {
-          
-      }
+  async getUserList() {
+    try {
+      let res = await userAPI.getUserList();
+      this.setState({
+        userList: res.data
+      });
+      console.log(this.state.userList, "adsdas");
+    } catch (error) {}
   }
 }
 
