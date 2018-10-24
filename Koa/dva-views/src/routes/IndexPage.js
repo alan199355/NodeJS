2babd882b668f26712b4ee9a685873e208a6dcd1
alan199355 <2271088240@qq.com>
2018-10-24 22:21:20 +0800

    tb

--- Koa/dva-views/src/routes/IndexPage.js
+++ Koa/dva-views/src/routes/IndexPage.js
@@ -5,11 +5,11 @@ import styles from "./IndexPage.css";
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+      <h1 className={styles.title}>Yay! Weadasdasdlcome to dva!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>
-          Tossssssadsadasd get started, edit <code>src/index.js</code> and save
+          Tosssadsadasd get started, edit <code>src/index.js</code> and save
           to reload.
         </li>
         <li>
