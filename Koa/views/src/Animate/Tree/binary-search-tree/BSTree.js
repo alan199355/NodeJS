a9e938a2fd953d7d8305031ed9e54a7556e075f6
alan199355 <2271088240@qq.com>
2020-03-01 23:18:57 +0800

    tb

--- Koa/views/src/Animate/Tree/binary-search-tree/BSTree.js
+++ Koa/views/src/Animate/Tree/binary-search-tree/BSTree.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'antd'
 import BinarySearchTree from './BinarySearchTree'
+import './BSTree.scss'
 class BSTree extends React.Component {
   constructor() {
     super()
@@ -15,6 +16,25 @@ class BSTree extends React.Component {
     let val = Math.floor(Math.random() * 100)
     let bsTree = this.state.bsTree
     bsTree.insert(val)
+    console.log(bsTree, val, bsTree.toString())
+    // canvas绘图
+    let canvas = document.getElementById('tree')
+    let ctx = canvas.getContext('2d')
+    //  计算画布的宽度
+    let width = canvas.offsetWidth
+    //  计算画布的高度
+    let height = canvas.offsetHeight
+    //  设置宽高
+    canvas.width = width
+    canvas.height = height
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.lineWidth = 1
+    ctx.beginPath()
+
+    ctx.arc(100, 75, 25, 0, 2 * Math.PI)
+    // ctx.lineTo(250,100)
+    ctx.stroke()
+
     this.setState({
       bsTree: bsTree
     })
@@ -32,12 +52,15 @@ class BSTree extends React.Component {
 
     return (
       <div className="tree-container">
-        <Button onClick={this.insertVal} type="primary">
-          插值
-        </Button>
-        <Button onClick={this.treeDetail} type="primary">
-          详情
-        </Button>
+        <div className="btn-group">
+          <Button onClick={this.insertVal} type="primary">
+            插值
+          </Button>
+          <Button onClick={this.treeDetail} type="primary">
+            详情
+          </Button>
+        </div>
+        <canvas id="tree"></canvas>
       </div>
     )
   }
