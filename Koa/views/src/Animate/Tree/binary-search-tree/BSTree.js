d9b9735467a83a5f8ffb8050992a8df9982cb4ce
alan199355 <2271088240@qq.com>
2020-03-02 22:42:36 +0800

    tb

--- Koa/views/src/Animate/Tree/binary-search-tree/BSTree.js
+++ Koa/views/src/Animate/Tree/binary-search-tree/BSTree.js
@@ -10,6 +10,9 @@ class BSTree extends React.Component {
     }
     this.insertVal = this.insertVal.bind(this)
     this.treeDetail = this.treeDetail.bind(this)
+    this.renderTree = this.renderTree.bind(this)
+    this.renderTreeNode = this.renderTreeNode.bind(this)
+    this.mapTree = this.mapTree.bind(this)
     let bsTree = new BinarySearchTree()
   }
   insertVal() {
@@ -17,6 +20,14 @@ class BSTree extends React.Component {
     let bsTree = this.state.bsTree
     bsTree.insert(val)
     console.log(bsTree, val, bsTree.toString())
+
+    this.setState({
+      bsTree: bsTree
+    })
+    this.renderTree()
+  }
+  // 绘制树图案
+  renderTree() {
     // canvas绘图
     let canvas = document.getElementById('tree')
     let ctx = canvas.getContext('2d')
@@ -29,19 +40,37 @@ class BSTree extends React.Component {
     canvas.height = height
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.lineWidth = 1
-    ctx.beginPath()
+    // ctx.beginPath()
+    let bsTree = this.state.bsTree
+    this.mapTree(bsTree.root, 1, 500, 80, 'root')
 
-    ctx.arc(100, 75, 25, 0, 2 * Math.PI)
+    ctx.font = '100px'
     // ctx.lineTo(250,100)
     ctx.stroke()
-
-    this.setState({
-      bsTree: bsTree
-    })
-    // let bsTree = new BinarySearchTree()
-    // for (let i = -0; i < 10; i++) {
-    //   bsTree.insert(i)
-    // }
+  }
+  mapTree(node, level, x, y, type) {
+    console.log(node, level, 'map')
+    let canvas = document.getElementById('tree')
+    let ctx = canvas.getContext('2d')
+    if (type === 'root') {
+      this.renderTreeNode(ctx, x, y, node.value)
+    } else if (type === 'left') {
+      x -= 100
+    } else if (type === 'right') {
+      x += 100
+    }
+    y += 50
+    this.renderTreeNode(ctx, x, y, node.value)
+    if (node.left) {
+      this.mapTree(node.left, level + 1, x, y, 'left')
+    }
+    if (node.right) {
+      this.mapTree(node.right, level + 1, x, y, 'right')
+    }
+  }
+  renderTreeNode(ctx, x, y, val) {
+    ctx.arc(x, y, 25, 0, 2 * Math.PI)
+    ctx.fillText(val, x - 5, y + 5)
   }
   treeDetail() {
     let bsTree = this.state.bsTree
