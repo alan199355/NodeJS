641038497b44a72947f860ed4485a73c729e8592
alan199355 <2271088240@qq.com>
2020-03-11 22:33:36 +0800

    同步

--- Koa/views/src/Animate/Tree/binary-search-tree/BSTree.js
+++ Koa/views/src/Animate/Tree/binary-search-tree/BSTree.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Button, InputNumber } from 'antd'
 import BinarySearchTree from './BinarySearchTree'
 import './BSTree.scss'
 class BSTree extends React.Component {
   constructor() {
     super()
     this.state = {
-      bsTree: new BinarySearchTree()
+      bsTree: new BinarySearchTree(),
+      val: 0
     }
     this.insertVal = this.insertVal.bind(this)
     this.treeDetail = this.treeDetail.bind(this)
@@ -14,7 +15,8 @@ class BSTree extends React.Component {
     this.renderTreeNode = this.renderTreeNode.bind(this)
     this.mapTree = this.mapTree.bind(this)
     this.reset = this.reset.bind(this)
-    let bsTree = new BinarySearchTree()
+    this.onChange = this.onChange.bind(this)
+    this.insertNum = this.insertNum.bind(this)
   }
   // 重置数据和canvas
   reset() {
@@ -89,26 +91,50 @@ class BSTree extends React.Component {
   renderTreeNode(ctx, x, y, val, type) {
     ctx.arc(x, y, 25, 0, 2 * Math.PI)
     // ctx.clearRect(x - 25, y - 25, 50, 50)
-    ctx.fillStyle = '#ff0000'
-    ctx.font = '80px #ff0000'
-    // ctx.strokeText(val, x - 5, y + 5)
-    ctx.fillText(val, x - 5, y + 5)
+
     ctx.stroke()
     ctx.closePath()
 
     ctx.beginPath()
-    ctx.moveTo(x, y)
+
     if (type === 'left') {
-      ctx.lineTo(x + 100, y - 50)
+      // 移动到当前节点位置
+      ctx.moveTo(x + 18, y - 18)
+      // 与父节点圆心位置画线
+      ctx.lineTo(x + 82, y - 32)
     } else if (type === 'right') {
-      ctx.lineTo(x - 100, y - 50)
+      // 移动到当前节点位置
+      ctx.moveTo(x - 18, y - 18)
+      // 与父节点圆心位置画线
+      ctx.lineTo(x - 82, y - 32)
     }
+    ctx.fillStyle = '#ff0000'
+    ctx.font = '80px #ff0000'
+    ctx.fillText(val, x - 5, y + 5)
     ctx.closePath()
     ctx.stroke()
   }
   treeDetail() {
     let bsTree = this.state.bsTree
     console.log(bsTree, bsTree.root, bsTree.toString())
+  }
+  onChange(val) {
+    this.setState({
+      val: val
+    })
+  }
+  insertNum() {
+    let val = this.state.val
+    let bsTree = this.state.bsTree
+    // 判断值是否已存在树中
+    if (!bsTree.contains(val)) {
+      bsTree.insert(val)
+    }
+
+    this.setState({
+      bsTree: bsTree
+    })
+    this.renderTree()
   }
   render() {
     // console.log(bsTree, 'tree')
@@ -124,6 +150,10 @@ class BSTree extends React.Component {
           </Button>
           <Button onClick={this.reset} type="primary">
             重置
+          </Button>
+          <InputNumber onChange={this.onChange} />
+          <Button onClick={this.insertNum} type="primary">
+            手动插值
           </Button>
         </div>
         <canvas id="tree"></canvas>
